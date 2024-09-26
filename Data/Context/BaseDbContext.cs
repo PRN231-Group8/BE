9a599bcdbedc2b616bdacabf3ae24d94b2c96a5f
HaiLeVN <lehai112003@gmail.com>
2024-09-26 11:09:55 +0700

    feat: fix identity user

--- Data/Context/BaseDbContext.cs
+++ Data/Context/BaseDbContext.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ExploreNow.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExploreNow.Data.Context
 {
-    public abstract class BaseDbContext : IdentityDbContext
+    public abstract class BaseDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
         protected BaseDbContext(DbContextOptions options)
             : base(options)
