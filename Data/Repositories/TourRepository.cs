aaf95dce3003dc0522d35937ca25767b19553839
thiennlse <thiennlse172858@gmail.com>
2024-10-05 14:37:10 +0700

    chore: move TourRepo

--- Data/Repositories/TourRepository.cs
+++ Data/Repositories/TourRepository.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PRN231.ExploreNow.BusinessObject.Entities;
-using PRN231.ExploreNow.Repositories.Repositories;
 using PRN231.ExploreNow.Repositories.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -8,9 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PRN231.ExploreNow.Repositories
+namespace PRN231.ExploreNow.Repositories.Repositories
 {
-    public class TourRepository : BaseRepository<Tour> ,ITourRepository
+    public class TourRepository : BaseRepository<Tour>, ITourRepository
     {
         private readonly BaseRepository<Tour> _baseRepository;
 
