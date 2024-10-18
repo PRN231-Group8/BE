e923d99e41f432b262ad17b4b33c8c87d0ade037
ZienMinh <MinhNLVSE172956@fpt.edu.vn>
2024-10-18 16:12:09 +0700

    fix: redefine the fields needed for the project

--- PRN231.ExploreNow.BusinessObject/Entities/Transaction.cs
+++ PRN231.ExploreNow.BusinessObject/Entities/Transaction.cs
@@ -1,8 +1,13 @@
-﻿namespace PRN231.ExploreNow.BusinessObject.Entities;
+﻿using PRN231.ExploreNow.BusinessObject.Enums;
+
+namespace PRN231.ExploreNow.BusinessObject.Entities;
 
 public class Transaction : BaseEntity
 {
-    public decimal Amount { get; set; }
-    public string UserId { get; set; }
-    public ApplicationUser User { get; set; }
+	public decimal Amount { get; set; }
+	public string UserId { get; set; }
+	public ApplicationUser User { get; set; }
+	public Guid PaymentId { get; set; }
+	public Payment Payment { get; set; }
+	public PaymentTransactionStatus Status { get; set; }
 }
