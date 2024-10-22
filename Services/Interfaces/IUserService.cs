399b58845876d07329434d30d5bd6237409d6af0
kieutranquocson5 <kieutranquocson3@gmail.com>
2024-10-22 08:35:13 +0700

    chore: change api name and delete trailing spaces

--- Services/Interfaces/IUserService.cs
+++ Services/Interfaces/IUserService.cs
@@ -10,11 +10,11 @@ using System.Threading.Tasks;
 
 namespace PRN231.ExploreNow.Services.Interfaces
 {
-    public interface IUserService
-    {
-        Task<bool> VerifyEmailTokenAsync(string email, string token);
-        Task<string> SaveImage(IFormFile image);
-        Task<UserProfileResponseModel> UpdateUserProfile(string id, UserProfileRequestModel profile);
+	public interface IUserService
+	{
+		Task<bool> VerifyEmailTokenAsync(string email, string token);
+		Task<string> SaveImage(IFormFile image);
+		Task<UserProfileResponseModel> UpdateUserProfile(string id, UserProfileRequestModel profile);
 		Task<UserProfileResponseModel> GetUserByEmailAsync(string email);
 	}
 }
