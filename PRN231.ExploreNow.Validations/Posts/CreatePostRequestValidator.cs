f2673fcbf0f28743a3a5c734e64ea1a242554dcc
HaiLeVN <lehai112003@gmail.com>
2024-11-07 15:13:54 +0700

    fefactor: namespace folder

--- PRN231.ExploreNow.Validations/Posts/CreatePostRequestValidator.cs
+++ PRN231.ExploreNow.Validations/Posts/CreatePostRequestValidator.cs
@@ -9,16 +9,16 @@ namespace PRN231.ExploreNow.Validations.Posts
         public CreatePostRequestValidator()
         {
             RuleFor(x => x.Content)
-                .NotEmpty().WithMessage("Content is required.")
-                .MaximumLength(1000).WithMessage("Content can't exceed 1000 characters.");
+                            .NotEmpty().WithMessage("Content is required.")
+                            .MaximumLength(1000).WithMessage("Content can't exceed 1000 characters.");
 
             RuleFor(x => x.Photos)
                 .NotEmpty().WithMessage("Please upload at least one image.")
                 .Must(photos => photos.Count <= 5).WithMessage("You can upload up to 5 images only.")
                 .ForEach(photo =>
                 {
-                    photo.Must(file => file.Length <= 3 * 1024 * 1024)
-                         .WithMessage("Each file must be smaller than 3MB.");
+                    photo.Must(file => file.Length <= 10 * 1024 * 1024)
+                         .WithMessage("Each file must be smaller than 10MB.");
                 });
         }
     }
