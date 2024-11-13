34b9e149e9e6df8556e23d84a7df23bbe10844fc
Styematic <128201525+Taikt08s@users.noreply.github.com>
2024-11-13 15:48:18 +0700

    fix: format code

--- PRN231.ExploreNow.BusinessObject/Entities/Posts.cs
+++ PRN231.ExploreNow.BusinessObject/Entities/Posts.cs
@@ -1,4 +1,4 @@
-﻿using PRN231.ExploreNow.BusinessObject.Enums;
+using PRN231.ExploreNow.BusinessObject.Enums;
 
 namespace PRN231.ExploreNow.BusinessObject.Entities;
 
@@ -6,12 +6,12 @@ public class Posts : BaseEntity
 {
     public string Title { get; set; }
     public string Content { get; set; }
-	public int Rating { get; set; }
-	public PostsStatus Status { get; set; } = PostsStatus.Pending;
-	public string UserId { get; set; }
-	public ApplicationUser User { get; set; }
-	public ICollection<Comments> Comments { get; set; } = new List<Comments>();
-	public ICollection<Photo> Photos { get; set; } = new List<Photo>();
+    public int Rating { get; set; }
+    public PostsStatus Status { get; set; } = PostsStatus.Pending;
+    public string UserId { get; set; }
+    public ApplicationUser User { get; set; }
+    public ICollection<Comments> Comments { get; set; } = new List<Comments>();
+    public ICollection<Photo> Photos { get; set; } = new List<Photo>();
     public bool IsRecommended { get; set; }
     public Guid TourTripId { get; set; }
     public TourTrip TourTrip { get; set; }
