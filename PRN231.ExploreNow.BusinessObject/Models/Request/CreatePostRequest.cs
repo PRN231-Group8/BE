645bc19b502cf0f534e6820d071489ab47716d93
Taikt08s <Xtrangemusic@gmail.com>
2024-11-13 15:34:21 +0700

    fix: remove trailling and reformat code

--- PRN231.ExploreNow.BusinessObject/Models/Request/CreatePostRequest.cs
+++ PRN231.ExploreNow.BusinessObject/Models/Request/CreatePostRequest.cs
@@ -5,11 +5,11 @@ using PRN231.ExploreNow.BusinessObject.Enums;
 
 namespace PRN231.ExploreNow.BusinessObject.Models.Request
 {
-	public class CreatePostRequest
-	{
+    public class CreatePostRequest
+    {
         public bool IsRecommended { get; set; }
         public string Content { get; set; }
-		public List<IFormFile> Photos { get; set; } = new List<IFormFile>();
+        public List<IFormFile> Photos { get; set; } = new List<IFormFile>();
         public Guid TourTripId { get; set; }
     }
 }
